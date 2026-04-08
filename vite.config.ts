236307f31d1c6353c234172b8ac68d21d7e69a36
yurij <mihov.yurij@gmail.com>
2026-04-09 00:07:42 +0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-
 import { defineConfig } from 'vitest/config';
-
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -9,13 +7,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
+  },
 });
-
-
-
-
-
-
-
-
