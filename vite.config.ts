9252dc1206f89a69913ca29254820e4557dd05df
yurij <mihov.yurij@gmail.com>
2026-04-09 01:02:19 +0300

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
   test: {
     globals: true,
     environment: 'jsdom',
